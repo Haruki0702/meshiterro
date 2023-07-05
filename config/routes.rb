2ae688c786a94a64b4a06e8d30a3619c43728347
Haruki0702 <23280696+Haruki0702@users.noreply.github.com>
2023-07-05 07:54:21 +0000

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
+  resources :users, only: [:show,:edit]
   root to: "homes#top"
+  get  'homes/about', to: 'homes#about',as: :about
+  delete "post_images/:id" => "post_images#destroy", as: "destroy_image"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
